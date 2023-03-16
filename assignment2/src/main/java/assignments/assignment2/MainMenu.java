4300d52770085f1293b2a209e9c60baf96f498bd
kkuratolodon <113555728+kkuratolodon@users.noreply.github.com>
2023-03-16 15:17:17 +0700

    membuat id nota yang di print adalah stringnya

--- assignment2/src/main/java/assignments/assignment2/MainMenu.java
+++ assignment2/src/main/java/assignments/assignment2/MainMenu.java
@@ -185,15 +185,15 @@ public class MainMenu {
         }
         // jika tidak ditemukan
         if(!found){
-            System.out.println("Nota dengan ID " + idNotaInt + " tidak ditemukan!");
+            System.out.println("Nota dengan ID " + idNota + " tidak ditemukan!");
             return;
         } 
         // jika ditemukan cek apakah sudah ready apa nggak
         if(!notaDiambil.getIsReady()){
-            System.out.println("Nota dengan ID " + idNotaInt + " gagal diambil!");
+            System.out.println("Nota dengan ID " + idNota + " gagal diambil!");
         } else{
             // jika sudah, hapus notanya dari list
-            System.out.println("Nota dengan ID " + idNotaInt + " berhasil diambil!");
+            System.out.println("Nota dengan ID " + idNota + " berhasil diambil!");
             notaList.remove(notaDiambil);
         }
     }
